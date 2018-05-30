730d2817018e6c3616461ee1504993f8fa64a2a0
richarddevilla <chaseyba@gmail.com>
2018-05-30 08:55:16 +0000

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,18 @@
+
+sudo pip3 install gtfs-realtime-bindings
+sudo pip3 install folium
+sudo pip3 install requests
+
+sudo apt-get install libgeos-c1v5
+
+sudo pip3 install shapely
+!!!!alternately
+
+wget https://files.pythonhosted.org/packages/7d/3c/0f09841db07aabf9cc387662be646f181d07ed196e6f60ce8be5f4a8f0bd/Shapely-1.6.4.post1.tar.gz
+tar xvzf Shapely-1.6.4.post1.tar.gz
+
+sudo mv /home/pi/Shapely-1.6.4.post1/shapely /usr/local/lib/python3.5/dist-packages
+!!!
+
+
+sudo apt-get install apache2 -y
--- bus.py
+++ bus.py
@@ -7,10 +7,9 @@ import folium
 import os.path
 from sense_hat import SenseHat
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = '/var/www/html/'
 db_path = os.path.join(BASE_DIR, "testTable")
-index_path = os.path.join(BASE_DIR, "index.html")
-map_path = os.path.join(BASE_DIR, "map.html")
+map_path = os.path.join(BASE_DIR, "index.html")
 
 
 sense = SenseHat()
@@ -19,6 +18,7 @@ def getVehiclePosition():
         api_key = 'cYYUJIy0P5wuNLTe5KPG1jtOKlL6VroLGN3Z'
         headers = {"Authorization":"apikey " + api_key}
         bus_positions = requests.get('https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/buses', headers=headers)
+        #Parse content to google protobuf
         feed = gtfs_realtime_pb2.FeedMessage()
         feed.ParseFromString(bus_positions.content)
         positions_output = [] # a list of lists for bus position data
@@ -32,29 +32,27 @@ def getVehiclePosition():
                             entity.vehicle.position.bearing,
                             entity.vehicle.position.speed,
                             entity.vehicle.trip.trip_id]
-                print(bus_info)
+                print(bus_info[0][-5:-2])
                 positions_output.append(bus_info)
+        if positions_output == []:
+            print('No BUS')
+            sense.load_image('GO.jpg')
+        else:
+            print('BUS Incoming')
+            sense.load_image('STOP.jpg')
         return positions_output
 
 
 #start of main script
 def createHTML(buses):
-    result=''
     vehiclemap = folium.Map(location=(-33.813108, 151.006816), zoom_start=20,tiles='Stamen Toner',width=750, height=500)
     for each in buses:
         vehiclemap.add_child(folium.Marker(location=[each[1],each[2]],
             popup='Bus '+ each[0][-5:-2] + ' going to ' + str(each[3]), icon=folium.Icon(color='green')))
     vehiclemap.save(outfile=map_path)
-    if result == '':
-            print('No HTML created')
-            sense.load_image('GO.jpg')
-    else:
-            print('HTML created')
-            sense.load_image('STOP.jpg')
-
+    
 
 parramatta = Polygon([(-33.813108, 151.006816), (-33.813065, 151.006726), (-33.815153, 151.006107), (-33.815121, 151.006034)])
-#parramatta = Polygon([(-33.816980,151.003976), (-33.817140,151.003819), (-33.818171,151.005522), (-33.818024,151.005668)])
 while True:
         try:
                 buses = getVehiclePosition()
